c0255c25aeca78f3d4a1368ece743876e7674e79
Gleb Buzin <qufiwefefwoyn@gmail.com>
2021-03-09 22:28:54 +0300

    Sit accusantium voluptatem consequatur aut perferendis.

--- main.go
+++ main.go
@@ -32,25 +32,27 @@ func main() {
 	defer fmt.Println("all done")
 
 	daysBack := 365
-	for i := 1; i < daysBack; i++ {
+	for i := daysBack; i >= 1; i-- {
 		fmt.Printf("commiting %d days ago\n", i)
-		changeTxt := faker.Paragraph() + "\n" + fmt.Sprint(rand.Int()) + "\n"
-		if err := os.WriteFile("tmp.txt", []byte(changeTxt), 0o777); err != nil {
-			log.Fatalln("writing tmp file failed: ", err)
-		}
-		if _, err := exec.Command("bash", bashArgs(add)...).Output(); err != nil {
-			log.Fatalln("git add failed: ", err)
-		}
+		for j := 0; j < 30; j++ {
+			changeTxt := faker.Paragraph() + "\n" + fmt.Sprint(rand.Int()) + "\n"
+			if err := os.WriteFile("tmp.txt", []byte(changeTxt), 0o777); err != nil {
+				log.Fatalln("writing tmp file failed: ", err)
+			}
+			if _, err := exec.Command("bash", bashArgs(add)...).Output(); err != nil {
+				log.Fatalln("git add failed: ", err)
+			}
 
-		day := time.Now().AddDate(0, 0, -i)
-		timeStr := day.Format("Mon, 02 Jan 2006 15:04:05 -0700")
-		os.Setenv("GIT_AUTHOR_DATE", timeStr)
-		os.Setenv("GIT_COMMITTER_DATE", timeStr)
+			day := time.Now().AddDate(0, 0, -i)
+			timeStr := day.Format("Mon, 02 Jan 2006 15:04:05 -0700")
+			os.Setenv("GIT_AUTHOR_DATE", timeStr)
+			os.Setenv("GIT_COMMITTER_DATE", timeStr)
 
-		commitMsg := faker.Sentence()
-		commitCmd := fmt.Sprintf(commit, commitMsg)
-		if _, err := exec.Command("bash", bashArgs(commitCmd)...).Output(); err != nil {
-			log.Fatalln("git commit failed: ", err)
+			commitMsg := faker.Sentence()
+			commitCmd := fmt.Sprintf(commit, commitMsg)
+			if _, err := exec.Command("bash", bashArgs(commitCmd)...).Output(); err != nil {
+				log.Fatalln("git commit failed: ", err)
+			}
 		}
 	}
 }

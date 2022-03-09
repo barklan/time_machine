97e6bc4908a7e3f98315c6016f1882e3d3b3210f
Gleb Buzin <qufiwefefwoyn@gmail.com>
2022-03-09 22:16:08 +0300

    add

--- main.go
+++ main.go
@@ -29,21 +29,28 @@ func bashArgs(cmd string) []string {
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	changeTxt := faker.Paragraph() + "\n" + fmt.Sprint(rand.Int()) + "\n"
-	if err := os.WriteFile("tmp.txt", []byte(changeTxt), 0o777); err != nil {
-		log.Fatalln("writing tmp file failed: ", err)
-	}
-	if _, err := exec.Command("bash", bashArgs(add)...).Output(); err != nil {
-		log.Fatalln("git add failed: ", err)
-	}
+	defer fmt.Println("all done")
 
-	day := time.Now().AddDate(0, 0, -1)
-	timeStr := day.Format("Mon, 02 Jan 2006 15:04:05 -0700")
-	os.Setenv("GIT_AUTHOR_DATE", timeStr)
-	os.Setenv("GIT_COMMITTER_DATE", timeStr)
+	daysBack := 5 * 365
+	for i := 1; i < daysBack; i++ {
+		fmt.Printf("commiting %d days ago", i)
+		changeTxt := faker.Paragraph() + "\n" + fmt.Sprint(rand.Int()) + "\n"
+		if err := os.WriteFile("tmp.txt", []byte(changeTxt), 0o777); err != nil {
+			log.Fatalln("writing tmp file failed: ", err)
+		}
+		if _, err := exec.Command("bash", bashArgs(add)...).Output(); err != nil {
+			log.Fatalln("git add failed: ", err)
+		}
 
-	commitCmd := fmt.Sprintf(commit, "hey!")
-	if _, err := exec.Command("bash", bashArgs(commitCmd)...).Output(); err != nil {
-		log.Fatalln("git commit failed: ", err)
+		day := time.Now().AddDate(0, 0, -1)
+		timeStr := day.Format("Mon, 02 Jan 2006 15:04:05 -0700")
+		os.Setenv("GIT_AUTHOR_DATE", timeStr)
+		os.Setenv("GIT_COMMITTER_DATE", timeStr)
+
+		commitMsg := faker.Sentence()
+		commitCmd := fmt.Sprintf(commit, commitMsg)
+		if _, err := exec.Command("bash", bashArgs(commitCmd)...).Output(); err != nil {
+			log.Fatalln("git commit failed: ", err)
+		}
 	}
 }
